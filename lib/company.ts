58c428becdc9a8f8c4fe932db0c40083d0ccb1e3
harishrvicfit-alt <harishrvicfit@gmail.com>
2026-06-30 13:31:33 +0200

    fix: correct company address spelling

--- lib/company.ts
+++ lib/company.ts
@@ -5,7 +5,7 @@ export const company = {
   projectSlug: "lbl-bau",
   logo: "/images/lbl-bau-logo.png",
   owner: "Zoran Katava",
-  street: "Adlsteiner Str. 9A",
+  street: "Adlsteiner Straße 9A",
   postalCode: "84494",
   city: "Neumarkt-Sankt Veit",
   country: "Deutschland",
@@ -15,7 +15,7 @@ export const company = {
   email: "info@lbl-bau.de",
   website: "https://www.lblbau.de",
   officeHours: "Termine nach Vereinbarung",
-  mapQuery: "LBL Bau, Adlsteiner Str. 9A, 84494 Neumarkt-Sankt Veit, Deutschland",
+  mapQuery: "LBL Bau, Adlsteiner Straße 9A, 84494 Neumarkt-Sankt Veit, Deutschland",
 };
 
 export const fullAddress = `${company.street}, ${company.postalCode} ${company.city}`;
